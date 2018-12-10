dc785117dfe8121b6617ac108c226f29322872bb
jem <michael.jerger@meissa-gmbh.de>
2018-12-10 23:40:50 +0100

    initial working integration tests

--- integration/docker/image/resources/test_it.sh
+++ integration/docker/image/resources/test_it.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 java -jar /app/dda-serverspec-standalone.jar /app/certificate-file.edn
+echo "----------"
 java -jar /app/dda-serverspec-standalone.jar /app/command.edn
+echo "----------"
 java -jar /app/dda-serverspec-standalone.jar /app/file.edn
-java -jar /app/dda-serverspec-standalone.jar /app/http-cert.edn
-java -jar /app/dda-serverspec-standalone.jar /app/netcat.edn
-java -jar /app/dda-serverspec-standalone.jar /app/netstat.edn
+echo "----------"
 java -jar /app/dda-serverspec-standalone.jar /app/package.edn
-java -jar /app/dda-serverspec-standalone.jar /app/iproute.edn
+echo "----------"
+java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/http-cert.edn
+java -jar /app/dda-serverspec-standalone.jar /app/http-cert.edn
+#echo "\\n\\n"
+#java -jar /app/dda-serverspec-standalone.jar /app/netstat.edn
+#echo "\\n\\n"
+#java -jar /app/dda-serverspec-standalone.jar /app/netcat.edn
+#echo "\\n\\n"
+#java -jar /app/dda-serverspec-standalone.jar /app/iproute.edn
