3b24ea57fc2a7112f83815422edf7f3e21510cd4
jem <michael.jerger@meissa-gmbh.de>
2018-12-11 18:15:49 +0100

    activated all test & adjust routing 2 travis

--- integration/docker/image/resources/test_it.sh
+++ integration/docker/image/resources/test_it.sh
@@ -5,22 +5,22 @@ service ssh start
 
 # run the tests - every test should result in "passed"
 cd /app && \
-# java -jar /app/dda-serverspec-standalone.jar /app/certificate-file.edn && \
-# echo "----------" && \
-# java -jar /app/dda-serverspec-standalone.jar /app/command.edn && \
-# echo "----------" && \
-# java -jar /app/dda-serverspec-standalone.jar /app/file.edn && \
-# echo "----------" && \
-# java -jar /app/dda-serverspec-standalone.jar /app/package.edn && \
-# echo "----------" && \
-# java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/http-cert.edn && \
-# java -jar /app/dda-serverspec-standalone.jar /app/http-cert.edn && \
-# echo "----------" && \
-# java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/netstat.edn && \
-# java -jar /app/dda-serverspec-standalone.jar /app/netstat.edn && \
-# echo "----------" && \
-# java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/netcat.edn && \
-# java -jar /app/dda-serverspec-standalone.jar /app/netcat.edn && \
-# echo "----------" && \
+java -jar /app/dda-serverspec-standalone.jar /app/certificate-file.edn && \
+echo "----------" && \
+java -jar /app/dda-serverspec-standalone.jar /app/command.edn && \
+echo "----------" && \
+java -jar /app/dda-serverspec-standalone.jar /app/file.edn && \
+echo "----------" && \
+java -jar /app/dda-serverspec-standalone.jar /app/package.edn && \
+echo "----------" && \
+java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/http-cert.edn && \
+java -jar /app/dda-serverspec-standalone.jar /app/http-cert.edn && \
+echo "----------" && \
+java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/netstat.edn && \
+java -jar /app/dda-serverspec-standalone.jar /app/netstat.edn && \
+echo "----------" && \
+java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/netcat.edn && \
+java -jar /app/dda-serverspec-standalone.jar /app/netcat.edn && \
+echo "----------" && \
 java -jar /app/dda-serverspec-standalone.jar --install-dependencies /app/iproute.edn && \
 java -jar /app/dda-serverspec-standalone.jar /app/iproute.edn -v
