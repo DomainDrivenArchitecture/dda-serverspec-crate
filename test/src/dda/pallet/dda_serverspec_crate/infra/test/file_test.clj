b1e9534663e41ee8730a703dc83099b5090bba96
jem <michael.jerger@meissa-gmbh.de>
2019-06-14 07:40:17 +0200

    refactored file-test toward datatest

--- test/src/dda/pallet/dda_serverspec_crate/infra/test/file_test.clj
+++ test/src/dda/pallet/dda_serverspec_crate/infra/test/file_test.clj
@@ -16,54 +16,10 @@
 
 (ns dda.pallet.dda-serverspec-crate.infra.test.file-test
   (:require
-    [clojure.test :refer :all]
-    [pallet.actions :as actions]
-    [dda.pallet.dda-serverspec-crate.infra.test.file :as sut]))
+   [clojure.test :refer :all]
+   [data-test :refer :all]
+   [dda.pallet.dda-serverspec-crate.infra.test.file :as sut]))
 
-
-(def test-config-1 {:_etc_hosts {:exist? true}
-                    :_root_.ssh_authorized_keys {:exist? false}})
-
-(def test-config-2 {:_etc_hosts {:exist? false}
-                    :_root_.ssh_authorized_keys {:exist? false}})
-
-(def test-config-3 {:_etc_hosts {:exist? false}
-                    :_root_.ssh_authorized_keys {:exist? true}})
-
-(def test-config-4 {:_etc_hosts {:exist? true :group "root"}
-                    :_root_.ssh_authorized_keys {:exist? true :user "root"}
-                    :_root_.profile {:mod "654"}})
-
-(def test-config-5 {:_etc_alink {:exist? true :type "l" :link-to "/etc/linktarget"}
-                    :_etc {:exist? true :type "d"}})
-
-
-
-(def input
-  {:_etc_hosts {:path "/etc/hosts" :fact-exist? true}
-   :_root_.ssh_authorized_keys {:path "/root/.ssh/authorized_keys" :fact-exist? false}})
-
-(def input-2
-  {:_etc_hosts {:path "/etc/hosts" :fact-exist? true :fact-user "root" :fact-group "root" :fact-mod "644"}
-   :_root_.ssh_authorized_keys {:path "/root/.ssh/authorized_keys" :fact-exist? true :fact-user "root" :fact-group "root" :fact-mod "644"}
-   :_root_.profile {:path "/root/.profile" :fact-user "root" :fact-group "root" :fact-mod "644"}})
-
-(def input-3
-  {:_etc_alink {:path "/etc/link" :fact-exist? true :fact-type "l" :fact-link-to "/etc/linktarget"}
-   :_etc {:path "/etc" :fact-exist? true :fact-type "d" :fact-link-to ""}})
-
-(deftest test-file-internal
- (testing
-   "test test-file-internal"
-    (is (= 0
-          (:no-failed (sut/test-file-internal {} input))))
-    (is (= 0
-          (:no-failed (sut/test-file-internal test-config-1 input))))
-    (is (= 1
-          (:no-failed (sut/test-file-internal test-config-2 input))))
-    (is (= 2
-          (:no-failed (sut/test-file-internal test-config-3 input))))
-    (is (= 1
-          (:no-failed (sut/test-file-internal test-config-4 input-2))))
-    (is (= 0
-          (:no-failed (sut/test-file-internal test-config-5 input-3))))))
+(defdatatest should-test-file [input expected]
+  (is (= expected
+         (sut/test-file-internal (:test-config input) (:fact-result input)))))
