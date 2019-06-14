da6647e7d7612ef7a6a550c76beeb3fc19dfa556
jem <michael.jerger@meissa-gmbh.de>
2019-06-14 13:52:43 +0200

    refactor netstat-test

--- test/src/dda/pallet/dda_serverspec_crate/infra/test/netstat_test.clj
+++ test/src/dda/pallet/dda_serverspec_crate/infra/test/netstat_test.clj
@@ -16,21 +16,9 @@
 
 (ns dda.pallet.dda-serverspec-crate.infra.test.netstat-test
   (:require
-    [clojure.test :refer :all]
-    [pallet.actions :as actions]
-    [dda.pallet.dda-serverspec-crate.infra.test.netstat :as sut]))
-
-(def test-config-1 {:not-running-service_11 {:port "11" :exp-proto "tcp" :running? true}
-                    :sshd_0.0.0.0_11 {:port "11" :ip "0.0.0.0" :exp-proto "tcp" :running? false}
-                    :sshd_22 {:port "22" :running? true}
-                    (keyword "apache2_80") {:port "80" :ip "::" :exp-proto "tcp6" :running? true}})
-(def test-config-2 {:not-running-service_0.0.0.1_11 {:port "11" :ip "0.0.0.1" :exp-proto "tcp" :running? true}
-                    (keyword "apache2_81") {:port "81" :ip "::" :exp-proto "tcp6" :running? true}
-                    :sshd_0.0.0.0_22 {:port "22" :ip "0.0.0.0" :exp-proto "tcp" :running? true}})
-(def test-config-3 {:not-running-service_0.0.0.1_11 {:port "11" :ip "0.0.0.1" :exp-proto "tcp" :running? false}
-                    (keyword "apache2_80") {:port "80" :ip "::" :exp-proto "tcp7" :running? false}})
-(def test-config-4 {(keyword "apache2_80") {:port "80" :ip "::" :running? true}
-                    :sshd____22 {:port "22" :ip "::" :exp-proto "tcp6" :running? true}})
+   [clojure.test :refer :all]
+   [data-test :refer :all]
+   [dda.pallet.dda-serverspec-crate.infra.test.netstat :as sut]))
 
 (def input
   '({:fact-foreign-adress ":::*",
@@ -67,33 +55,6 @@
      :fact-send-q "0",
      :fact-user "0"}))
 
-(deftest test-netstat-internal
- (testing
-   "test test-netstat-internal"
-   (is (= 0
-          (:no-failed (sut/test-netstat-internal {} input))))
-   (is (= 1
-          (:no-failed (sut/test-netstat-internal test-config-1 input))))
-   (is (= 2
-          (:no-failed (sut/test-netstat-internal test-config-2 input))))
-   (is (= 0
-          (:no-failed (sut/test-netstat-internal test-config-3 input))))
-   (is (= 0
-          (:no-failed (sut/test-netstat-internal test-config-4 input))))))
-
-(deftest retest-issues
- (testing
-   "test issues"
-   (is (= 1
-          (:no-failed (sut/test-netstat-internal
-                       {:sshd_22 {:port 22}}
-                       '({:fact-foreign-adress "0.0.0.0:*", :fact-ip "0.0.0.0" :fact-port "22",
-                          :fact-recv-q "0", :fact-inode "15261", :fact-state "LISTEN",
-                          :fact-proto "tcp", :fact-pid "1200", :fact-process-name "sshd",
-                          :fact-send-q "0", :fact-user "0"}
-                         {:fact-foreign-adress ":::*", :fact-ip "::" :fact-port "22", :fact-recv-q "0",
-                          :fact-inode "15263", :fact-state "LISTEN", :fact-proto "tcp6", :fact-pid "1200",
-                          :fact-process-name "sshd", :fact-send-q "0", :fact-user "0"}
-                         {:fact-proto "udp", :fact-recv-q "0", :fact-send-q "0",
-                          :fact-ip "0.0.0.0" :fact-port "68", :fact-foreign-adress "0.0.0.0:*",
-                          :fact-state "0", :fact-user "13122", :fact-inode "919", :fact-pid "dhclient"})))))))
+(defdatatest should-test-netstat [input expected]
+  (is (= expected
+         (sut/test-netstat-internal (:test-config input) (:fact-result input)))))
